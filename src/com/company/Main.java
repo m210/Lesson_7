b2f4c927dcdaa414b5fe99a4357ce2b5c7cb1df4
m210 <m210-2007@mail.ru>
2021-12-06 15:02:23 +0300

    Completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String firstNamme = "Alexander";
+        String middleName ="Sergeevich";
+        String lastName = "Makarov";
+
+        String fullName = lastName + " " + firstNamme + " " + middleName;
+        System.out.println("ФИО: " + fullName);
+
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника заполения отчета - " + upperFullName);
+
+        String newFullName = fullName.replace(" ", "; ");
+        System.out.println("Данные ФИО сотрудника для административного отдела - " + newFullName);
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника - " + fullName);
     }
 }
